a710e6726db1e113f8db72bd9e5258e14ecf260d
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-17 21:43:42 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,7 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-x = 0
-while x < collection.length
+  x = 0
+  new_array = []
+    while x < collection.length
+    new_array << yield(collection[x])
+    x += 1
+  end
 
 end
